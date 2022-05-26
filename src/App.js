d8acabfa87cc27b506bcc32796419d44ea7ee5ad
alexfoster1022 <alexander.f.holman@gmail.com>
2022-05-26 15:30:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const History = (props) => {
+    if(props.allClicks.length === 0) {
+        return (
+            <div>
+                Use the app by pressing the buttons
+            </div>
+        )
+    }
+    return (
+        <div>
+            Button press history {props.allClicks.join(' ')}
+        </div>
+    )
+}
+
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
+const App = () => {
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+      setAll(allClicks.concat('L'));
+      setLeft(left + 1);
+  }
+  const handleRightClick = () => {
+      setAll(allClicks.concat('R'));
+      setRight(right + 1);
+  }
+
+    return (
+        <div>
+         {left}
+         <Button handleClick={handleLeftClick} text="Left" />
+         <Button handleClick={handleRightClick} text="Right" />
+         {right}
+        <History allClicks={allClicks} />
+        <button onClick={() => [setLeft(0), setRight(0), setAll([])]}>
+            Clear
+        </button>
+        
+        </div>
+    )
+
+
 }
 
 export default App;
+
+
+
+
